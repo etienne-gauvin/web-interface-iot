20307f15eacb3f08ebddfd39dd4307bef8ae60a1
Étienne Gauvin <etienne@gauvin.info>
2016-12-10 23:58:38 +0100

    Added graph (with Chart.js)

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,14 +1,49 @@
+"use strict";
+
 /**
  * Web Interface IoT Client
  */
 
 (() => {
 	
-	var socket = io()
+	const canvas = document.getElementById('chart')
+	const ctx = canvas.getContext('2d')
 	
-	socket.on('data', (data) => {
+	const allData = []
+	const allLabels = []
+	
+	const chart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: allLabels,
+	        datasets: [{
+	            label: "Temperature (°C)",
+	            data: allData
+	        }]
+	    },
+	    options: {
+	        responsive: true
+	    }
+	})
+	
+	const socket = io()
+	
+	socket.on('data', (newData) => {
 		
-	    console.info('Data update !', data)
+	    console.info('Data update !', newData)
+    	
+    	// Empty the arrays
+    	allData.splice(0, allData.length)
+    	allLabels.splice(0, allLabels.length)
+	    
+	    for (let nd in newData) {
+	    	
+		    allData.push(newData[nd].temp)
+		    allLabels.push(newData[nd].tdate)
+		    
+	    }
+	    
+	    chart.update()
 	
 	})
 	
