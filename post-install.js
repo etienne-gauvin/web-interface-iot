b0d0c7d78366c3f70bfa28f54eea6955683148e0
Étienne Gauvin <etienne@gauvin.info>
2016-12-10 23:04:16 +0100

    Updated post-install to keep old values of config.json

--- post-install.js
+++ post-install.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const fs = require('fs')
 
 const configTemplate = {
@@ -6,13 +8,63 @@ const configTemplate = {
 		"user"     : "root",
 		"password" : "",
 		"database" : "my_db"
-	}	
+	},
+	
+	"interval": 300
 }
 
+// If config.json does not exists
 if (! fs.existsSync('config.json')) {
 	
 	fs.writeFileSync('config.json', JSON.stringify(configTemplate, null, '\t'))
 	
-	console.info("Make sure to set your database configuration in config.json")
+	console.info("config.json has been created (make sure to set your database configuration in it)")
 	
+}
+
+// If config.json already exists
+else {
+	
+	const config = JSON.parse(fs.readFileSync('config.json'))
+	
+	// Upgrading the config
+	const newConfig = recursiveUpgrade(config, configTemplate)
+	
+	fs.writeFileSync('config.json', JSON.stringify(newConfig, null, '\t'))
+	
+	console.info("config.json has been upgraded")
+	
+}
+
+/**
+ * Upgrade the object
+ * (adding new keys, but keeping old values if already exists)
+ * @param <Object> oldConfig
+ * @param <Object> newConfig
+ * @return <Object>
+ */
+function recursiveUpgrade(oldConfig, newConfig) {
+	
+	for (let key in oldConfig) {
+		
+		if (newConfig[key] !== undefined) {
+			
+			if (typeof oldConfig[key] === 'object') {
+				
+				newConfig[key] = recursiveUpgrade(oldConfig[key], newConfig[key])
+				
+			}
+			
+			else {
+				
+				newConfig[key] = oldConfig[key]
+				
+			}
+			
+		}
+			
+	}
+	
+	return newConfig
+
 }
